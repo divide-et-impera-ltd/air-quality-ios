b06035819dc2419aca3d27526400b2e7c4969493
Razvan Rujoiu <rjrazvan@icloud.com>
2021-02-01 17:35:07 +0200

    Initial Commit

---
+++ AirQuality/AirQualityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirQualityApp.swift
+//  AirQuality
+//
+//  Created by Razvan Rujoiu on 01.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct AirQualityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirQuality/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AirQuality
+//
+//  Created by Razvan Rujoiu on 01.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
